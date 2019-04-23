928e304911a6869da90c05d9342dd465c524b8fc
AdrianDtt90 <adridotta@gmail.com>
2019-04-23 23:35:10 +1000

    All done, read Instrucciones.txt

---
+++ src/Entity/ToDo.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Core\Annotation\ApiResource;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ApiResource(iri="http://schema.org/ToDo")
+ * @ORM\Entity(repositoryClass="App\Repository\ToDoRepository")
+ */
+class ToDo
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $apellido;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $edad;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getApellido(): ?string
+    {
+        return $this->apellido;
+    }
+
+    public function setApellido(string $apellido): self
+    {
+        $this->apellido = $apellido;
+
+        return $this;
+    }
+
+    public function getEdad(): ?int
+    {
+        return $this->edad;
+    }
+
+    public function setEdad(int $edad): self
+    {
+        $this->edad = $edad;
+
+        return $this;
+    }
+}
